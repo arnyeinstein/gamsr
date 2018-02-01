bbfcebead7d5ba84d1b3fbbba9acb46f7909604c
Renger van Nieuwkoop <renger@modelworks.ch>
2018-02-01 10:20:35 +0100

    changed readme file and added a big file to test

--- gdxtext.R
+++ gdxtext.R
@@ -1,48 +1,76 @@
 l.all <- list()
-i <- gdxrrw::rgdx.set('./trnsport.gdx', 'i', compress = TRUE )
- attributes(i)$type <- 'parameter'
- l.all[[1]] <- list(name ='i', data = i, description = 'canning plants', type = 'set')
- names(l.all)[1] ='i'
- names(i) <- gsub(x = names(i), pattern = '*', replacement = 'index', fixed=TRUE)
-j <- gdxrrw::rgdx.set('./trnsport.gdx', 'j', compress = TRUE )
- attributes(j)$type <- 'parameter'
- l.all[[2]] <- list(name ='j', data = j, description = 'markets', type = 'set')
- names(l.all)[2] ='j'
- names(j) <- gsub(x = names(j), pattern = '*', replacement = 'index', fixed=TRUE)
-a <- tibble::as_data_frame(gdxrrw::rgdx.param('./trnsport.gdx', 'a', compress = TRUE, squeeze = FALSE))
- attributes(a)$type <- 'parameter'
- l.all[[3]] <- list(name ='a', data = a, description = 'capacity of plant i in cases', type = 'parameter')
- names(l.all)[3] ='a'
- names(a) <- gsub(x = names(a), pattern = '*', replacement = 'index', fixed=TRUE)
-b <- tibble::as_data_frame(gdxrrw::rgdx.param('./trnsport.gdx', 'b', compress = TRUE, squeeze = FALSE))
- attributes(b)$type <- 'parameter'
- l.all[[4]] <- list(name ='b', data = b, description = 'demand at market j in cases', type = 'parameter')
- names(l.all)[4] ='b'
- names(b) <- gsub(x = names(b), pattern = '*', replacement = 'index', fixed=TRUE)
-d <- tibble::as_data_frame(gdxrrw::rgdx.param('./trnsport.gdx', 'd', compress = TRUE, squeeze = FALSE))
- attributes(d)$type <- 'parameter'
- l.all[[5]] <- list(name ='d', data = d, description = 'distance in thousands of miles', type = 'parameter')
- names(l.all)[5] ='d'
- names(d) <- gsub(x = names(d), pattern = '*', replacement = 'index', fixed=TRUE)
-f <- gdxrrw::rgdx.scalar('./trnsport.gdx', 'f')
- attributes(f)$type <- 'scalar'
- l.all[[6]] <- list(name ='f', data = f, description = 'freight in dollars per case per thousand miles', type = 'parameter')
- names(l.all)[6] ='f'
- names(f) <- gsub(x = names(f), pattern = '*', replacement = 'index', fixed=TRUE)
-c <- tibble::as_data_frame(gdxrrw::rgdx.param('./trnsport.gdx', 'c', compress = TRUE, squeeze = FALSE))
- attributes(c)$type <- 'parameter'
- l.all[[7]] <- list(name ='c', data = c, description = 'transport cost in thousands of dollars per case', type = 'parameter')
- names(l.all)[7] ='c'
- names(c) <- gsub(x = names(c), pattern = '*', replacement = 'index', fixed=TRUE)
-x <- gdxrrw::rgdx("./trnsport.gdx", list(name ="x", field = "all"))
- attributes(x)$type <- 'variable'
-x <- tibble::as_data_frame(rgdx.var(x))
- l.all[[8]] <- list(name ='x', data = x, description = 'shipment quantities in cases', type = 'variable')
- names(l.all)[8] ='x'
- names(x) <- gsub(x = names(x), pattern = '*', replacement = 'index', fixed=TRUE)
-z <- gdxrrw::rgdx("./trnsport.gdx", list(name ="z", field = "all"))
- attributes(z)$type <- 'variable'
-z <- tibble::as_data_frame(rgdx.var(z))
- l.all[[9]] <- list(name ='z', data = z, description = 'total transportation costs in thousands of dollars', type = 'variable')
- names(l.all)[9] ='z'
- names(z) <- gsub(x = names(z), pattern = '*', replacement = 'index', fixed=TRUE)
+Merged_set_1 <- gdxrrw::rgdx.set('./merged.gdx', 'Merged_set_1', compress = TRUE )
+ attributes(Merged_set_1)$type <- 'parameter'
+ l.all[[1]] <- list(name ='Merged_set_1', data = Merged_set_1, description = 'Merge set', type = 'set')
+ names(l.all)[1] ='Merged_set_1'
+ names(Merged_set_1) <- gsub(x = names(Merged_set_1), pattern = '*', replacement = 'index', fixed=TRUE)
+results_gdp <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_gdp', compress = TRUE, squeeze = FALSE))
+ attributes(results_gdp)$type <- 'parameter'
+ l.all[[2]] <- list(name ='results_gdp', data = results_gdp, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[2] ='results_gdp'
+ names(results_gdp) <- gsub(x = names(results_gdp), pattern = '*', replacement = 'index', fixed=TRUE)
+results_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_r)$type <- 'parameter'
+ l.all[[3]] <- list(name ='results_r', data = results_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[3] ='results_r'
+ names(results_r) <- gsub(x = names(results_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_r_reg <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_r_reg', compress = TRUE, squeeze = FALSE))
+ attributes(results_r_reg)$type <- 'parameter'
+ l.all[[4]] <- list(name ='results_r_reg', data = results_r_reg, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[4] ='results_r_reg'
+ names(results_r_reg) <- gsub(x = names(results_r_reg), pattern = '*', replacement = 'index', fixed=TRUE)
+results_r_h <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_r_h', compress = TRUE, squeeze = FALSE))
+ attributes(results_r_h)$type <- 'parameter'
+ l.all[[5]] <- list(name ='results_r_h', data = results_r_h, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[5] ='results_r_h'
+ names(results_r_h) <- gsub(x = names(results_r_h), pattern = '*', replacement = 'index', fixed=TRUE)
+results_a_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_a_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_a_r)$type <- 'parameter'
+ l.all[[6]] <- list(name ='results_a_r', data = results_a_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[6] ='results_a_r'
+ names(results_a_r) <- gsub(x = names(results_a_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_f_a_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_f_a_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_f_a_r)$type <- 'parameter'
+ l.all[[7]] <- list(name ='results_f_a_r', data = results_f_a_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[7] ='results_f_a_r'
+ names(results_f_a_r) <- gsub(x = names(results_f_a_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_f_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_f_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_f_r)$type <- 'parameter'
+ l.all[[8]] <- list(name ='results_f_r', data = results_f_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[8] ='results_f_r'
+ names(results_f_r) <- gsub(x = names(results_f_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_a_c_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_a_c_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_a_c_r)$type <- 'parameter'
+ l.all[[9]] <- list(name ='results_a_c_r', data = results_a_c_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[9] ='results_a_c_r'
+ names(results_a_c_r) <- gsub(x = names(results_a_c_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_c_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_c_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_c_r)$type <- 'parameter'
+ l.all[[10]] <- list(name ='results_c_r', data = results_c_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[10] ='results_c_r'
+ names(results_c_r) <- gsub(x = names(results_c_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_ins_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_ins_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_ins_r)$type <- 'parameter'
+ l.all[[11]] <- list(name ='results_ins_r', data = results_ins_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[11] ='results_ins_r'
+ names(results_ins_r) <- gsub(x = names(results_ins_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_ins_f_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_ins_f_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_ins_f_r)$type <- 'parameter'
+ l.all[[12]] <- list(name ='results_ins_f_r', data = results_ins_f_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[12] ='results_ins_f_r'
+ names(results_ins_f_r) <- gsub(x = names(results_ins_f_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_c_h_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_c_h_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_c_h_r)$type <- 'parameter'
+ l.all[[13]] <- list(name ='results_c_h_r', data = results_c_h_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[13] ='results_c_h_r'
+ names(results_c_h_r) <- gsub(x = names(results_c_h_r), pattern = '*', replacement = 'index', fixed=TRUE)
+results_c_r_reg <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_c_r_reg', compress = TRUE, squeeze = FALSE))
+ attributes(results_c_r_reg)$type <- 'parameter'
+ l.all[[14]] <- list(name ='results_c_r_reg', data = results_c_r_reg, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[14] ='results_c_r_reg'
+ names(results_c_r_reg) <- gsub(x = names(results_c_r_reg), pattern = '*', replacement = 'index', fixed=TRUE)
+results_ins_insp_r <- tibble::as_data_frame(gdxrrw::rgdx.param('./merged.gdx', 'results_ins_insp_r', compress = TRUE, squeeze = FALSE))
+ attributes(results_ins_insp_r)$type <- 'parameter'
+ l.all[[15]] <- list(name ='results_ins_insp_r', data = results_ins_insp_r, description = 'Warning: no description', type = 'parameter')
+ names(l.all)[15] ='results_ins_insp_r'
+ names(results_ins_insp_r) <- gsub(x = names(results_ins_insp_r), pattern = '*', replacement = 'index', fixed=TRUE)
